b18fae5c5c2d038f00d33eb4141a4e45b0dfd19d
kome0724 <kome_engineer@ymobile.ne.jp>
2020-05-28 20:20:06 +0900

    error

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,20 +9,32 @@
 
 @section('content')
 <p>{{ $msg }}</p>
+@if(count($errors) > 0)
+<div>
+	<ul>
+		@foreach ($errors->all() as $error)
+				<li>{{ $error }}</li>
+		@endforeach
+	</ul>
+</div>
+@endif
 <form action="/hello" method="post">
     @csrf
     <tr>
-        <th>name:</th>
-        <td><input type="text" name="name"></td>
-    </tr>
+        <th>name: </th>
+        <td><input type="text" name="name" value="{{ old('name') }}"></td>
+		</tr>
+		<br>
     <tr>
-        <th>mail:</th>
-        <td><input type="text" name="mail"></td>
-    </tr>
+        <th>mail: </th>
+        <td><input type="text" name="mail" value="{{ old('mail') }}"></td>
+		</tr>
+		<br>
     <tr>
-        <th>age:</th>
-        <td><input type="text" name="age"></td>
-    </tr>
+        <th>age: </th>
+        <td><input type="text" name="age" value="{{ old('age') }}"></td>
+		</tr>
+		<br>
     <tr>
         <th></th>
         <td><input type="submit" value="send"></td>
