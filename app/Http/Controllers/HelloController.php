3ae20faa947f31f8c85ed158ff1d149a70c3d4bd
kome0724 <kome_engineer@ymobile.ne.jp>
2020-07-02 21:47:25 +0900

    クエリーバリデータ

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,16 +5,36 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
 	{
-		return view('hello.index', ['msg'=>'フォーム入力：']);
+        $validator = Validator::make($request->query(),[
+            'id'=>'required',
+            'pass'=>'required',
+        ]);
+        if($validator->fails()){
+            $msg = 'クエリーに問題があります。';
+        }else{
+            $msg = 'ID/PASSを受け付けました。フォームを入力してください。';
+        }
+		return view('hello.index', ['msg'=>$msg,]);
     }
 
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
+        $validator = Validator::make($request->all(),[
+            'name'=>'required',
+            'mail'=>'email',
+            'age'=>'numeric|between:0,150',
+        ]);
+        if($validator->fails()){
+            return redirect('/hello')
+                ->withErrors($validator)
+                ->withInput();
+        }
         return view('hello.index',['msg'=>'正しく入力されました']);
     }
 }
